73ed0eea1038a7343a6f05f356eae83a32e46525
Ken Barber <ken@bob.sh>
2011-06-22 17:14:05 +0200

    Fix user and ssh key validation for oneadmin_pubkey_rsa properly.

--- lib/facter/oneadmin_pubkey_rsa.rb
+++ lib/facter/oneadmin_pubkey_rsa.rb
@@ -1,14 +1,19 @@
 require 'etc'
 
-ent = Etc.getpwnam("oneadmin")
-if File.exists?(ent.dir + "/.ssh/id_rsa.pub") then
+Facter.add(:oneadmin_pubkey_rsa) do
+  setcode do
+    # First try to grab an entry for the onadmin user from /etc/passwd
+    begin
+      ent = Etc.getpwnam("oneadmin")
+    rescue Exception => e
+      exit(0)
+    end
 
-  Facter.add(:oneadmin_pubkey_rsa) do
-    setcode do
+    # Now grab the users public key and return the key part of it
+    if File.exists?(ent.dir + "/.ssh/id_rsa.pub") then
       keyfile = File.open(ent.dir + "/.ssh/id_rsa.pub")
       keyarray = keyfile.read.split(" ")
       keyarray[1]
     end
   end
-
 end
